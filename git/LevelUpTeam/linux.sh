813e38586bf293f5dd9e510c0dd0d1d0a36a1814
abach06 <abach06@gmail.com>
2022-12-05 06:31:36 +0000

    altered week 4 project files

---
+++ git/LevelUpTeam/linux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Please enter your name:"
+read  Aaron Bachman
+echo "Reading.... please wait"
+sleep 3 
+echo "-----------------------------"
+echo "Your username is:" && abach06 
+echo "-----------------------------"
+echo "This is one of my first Linux Scripts"
+sleep 2
+echo "Please wait while we process the date......."
+sleep 3
+echo "Made on:" && 12/03/2022
+echo "-----------------------------"
+sleep 2
+echo "My IP address is:"
+echo "-----------------------------"
+sleep 2
+curl ifconfig.me
+
+This is week 4 - project test
+
+Hey Red Team!
